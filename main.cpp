80d401f734ed829cbc920519c6215ca476129f9b
mandragora-hub <royer01gomez@gmail.com>
2025-05-23 23:22:12 -0400

    save-progress

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "spmonitorapp.h"
 
 int main(int argc, char* argv[]) {
-  // Since this example is running uninstalled, we have to help it find its
-  // schema. This is *not* necessary in a properly installed application.
+#if HAS_SEARCH_ENTRY2
+  // A GtkSearchEntry (e.g from window.ui) shall  be wrapped in
+  // a GtkSearchEntry2 and not in a deprecated Gtk::SearchEntry.
+  Gtk::Application::wrap_in_search_entry2(true);
+#endif
+  // Since this example is running uninstalled, we have to help it find
+  // its schema. This is *not* necessary in a properly installed
+  // application.
   Glib::setenv("GSETTINGS_SCHEMA_DIR", ".", false);
 
   auto app = SPMonitorApp::create();
--- spmonitorappwindow.h
+++ spmonitorappwindow.h
@@ -3,6 +3,8 @@
 
 #include <gtkmm-4.0/gtkmm.h>
 
+#define HAS_SEARCH_ENTRY2 GTKMM_CHECK_VERSION(4, 13, 2)
+
 class SPMonitorAppWindow : public Gtk::ApplicationWindow {
  public:
   SPMonitorAppWindow(BaseObjectType *cobject,
@@ -13,10 +15,28 @@ class SPMonitorAppWindow : public Gtk::ApplicationWindow {
   void open_file_view(const Glib::RefPtr<Gio::File> &file);
 
  protected:
+  // signal handlers:
+  void on_search_text_changed();
+  void on_visible_child_changed();
+  void on_find_word(const Gtk::Button *button);
+  void on_reveal_child_changed();
+
+  void update_words();
+
   Glib::RefPtr<Gtk::Builder> m_refBuilder;
   Glib::RefPtr<Gio::Settings> m_settings;
-  Gtk::Stack *m_stack {nullptr};
-  Gtk::MenuButton *m_gears {nullptr};
+  Gtk::Stack *m_stack{nullptr};
+  Gtk::MenuButton *m_gears{nullptr};
+  Gtk::ToggleButton *m_search{nullptr};
+  Gtk::SearchBar *m_searchbar{nullptr};
+#if HAS_SEARCH_ENTRY2
+  Gtk::SearchEntry2 *m_searchentry{nullptr};
+#else
+  Gtk::SearchEntry *m_searchentry{nullptr};
+#endif
+  Gtk::Revealer *m_sidebar{nullptr};
+  Gtk::ListBox *m_words{nullptr};
+  Glib::RefPtr<Glib::Binding> m_prop_binding;
 };
 
 #endif  // ARDUINO_SERIAL_PORT_COMMUNICATION_WINDOW_H
