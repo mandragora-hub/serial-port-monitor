be0ffa9cbc0f4dd80669db5104a59d0d2e484826
mandragora-hub <royer01gomez@gmail.com>
2025-08-08 23:18:23 -0400

    fix: Fine-tuning build and installation process

--- lib/external-dependencies.cmake
+++ lib/external-dependencies.cmake
@@ -7,7 +7,11 @@ ExternalProject_Add(libserialport
   GIT_REPOSITORY    https://github.com/mandragora-hub/libserialport.git
   GIT_TAG           origin/master
   PREFIX "${EXTERNAL_PROJECT_PREFIX}"
+  # INSTALL_DIR "libserialport"
+  # INSTALL_COMMAND "make install DESTDIR=./lsp"
   CONFIGURE_COMMAND <SOURCE_DIR>/autogen.sh
                     COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
+  UPDATE_DISCONNECTED 1      # Don’t re-fetch every time
+  # BUILD_BYPRODUCTS ${LIBSERIALPORT_PREFIX}/lib/libserialport.a
 )
 
